b3e21c1436fa9b0642cc29107b2e915fcbd5f68e
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-02-16 18:08:03 +0300

    Update installed.sh

--- installed.sh
+++ installed.sh
@@ -6,6 +6,6 @@ apt-get update &>/dev/null
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
 apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
-curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
+curl -o run-insta.py https://raw.githubusercontent.com/ahmad1abbadi/test2/main/run-insta.py && python3 run-insta.py
     exit
 fi
