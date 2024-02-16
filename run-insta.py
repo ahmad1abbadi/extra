c7e34e1893a9018a495e6315b9119e6ce9ad6057
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-02-16 18:09:41 +0300

    Update run-insta.py

--- run-insta.py
+++ run-insta.py
@@ -1,5 +1,5 @@
 import os
-os.system("wget https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/run-darkos.py &>/dev/null")   
+os.system("wget https://raw.githubusercontent.com/ahmad1abbadi/test2/main/run &>/dev/null")   
 os.system("wget https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/run-update &>/dev/null")
 os.system("chmod +x darkos")
 os.system("mv darkos darkos.py $PREFIX/bin/")
