fdc13177f3217538be745a9c6762dc1289ae896e
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-02-16 18:01:10 +0300

    Create installed.sh

---
+++ installed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+echo "installing packages please wait"
+echo ""
+apt-get update &>/dev/null
+apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
+apt install python --no-install-recommends -y &>/dev/null
+echo "be patient"
+curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
+    exit
+fi
