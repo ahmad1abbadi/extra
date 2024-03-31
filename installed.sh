5e608270793fdd4b18f487d6de5eb9cb986ebf04
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-03-31 23:23:04 +0300

    Update installed.sh

--- installed.sh
+++ installed.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-clear
-echo "installing packages please wait"
-echo ""
-apt-get update &>/dev/null
-apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
-apt install python --no-install-recommends -y &>/dev/null
-echo "be patient"
-curl -o run-insta.py https://raw.githubusercontent.com/ahmad1abbadi/test2/main/run-insta.py && python3 run-insta.py
-    exit
-fi
+
